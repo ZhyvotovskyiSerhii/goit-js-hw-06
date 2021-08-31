28cb223723440e2d20f00d350dfe1e7d269757f8
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 10:29:49 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,10 @@
-<<<<<<< Updated upstream
 const inputName = document.querySelector("#name-input");
 const inputAdd = document.querySelector("#name-output");
 inputName.addEventListener("input", (event) => {
     if (event.currentTarget.value !== "") {
-        document.querySelector('#name-output').textContent = event.currentTarget.value;
+        inputAdd.textContent = event.currentTarget.value;
       }
       else {
         document.querySelector('#name-output').textContent = "Anonymous";
       }
-=======
-const inputName = document.querySelector("#name-input");
-const inputAdd = document.querySelector("#name-output");
-inputName.addEventListener("input", (event) => {
-    if (event.currentTarget.value !== "") {
-      inputAdd.textContent = event.currentTarget.value;
-      }
-      else {
-        inputAdd.textContent = "Anonymous";
-      }
->>>>>>> Stashed changes
     })
