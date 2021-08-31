8a31acbf3ce31d155870d3b28ad801e4be148a9a
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 10:21:24 +0300

    Add files via upload

---
+++ js/task-02.js
@@ -0,0 +1,36 @@
+<<<<<<< Updated upstream
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsList = ingredients.map(ingredient => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem;
+})
+const ingredientsUl = document.querySelector("#ingredients");
+ingredientsUl.append(...ingredientsList);
+=======
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsList = ingredients.map(ingredient => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem;
+})
+const ingredientsUl = document.querySelector("#ingredients");
+ingredientsUl.append(...ingredientsList);
+>>>>>>> Stashed changes
+console.log(ingredientsUl);
