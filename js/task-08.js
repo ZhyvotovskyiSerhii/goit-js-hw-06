3c3cd39cfdee54578e844e785237e36c4643cedc
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 10:34:31 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,33 +1,3 @@
-<<<<<<< Updated upstream
-const forms = {
-    form: document.querySelector(".login-form"),
-  };
-  
-  const form1 = {};
-  
-  forms.form.addEventListener("submit", onFormSubmit);
-  
-  function onFormSubmit(e) {
-    e.preventDefault();
-  
-    for (let i = 0; i < e.currentTarget.elements.length - 1; i += 1) {
-      if (forms.form.elements[i].value === "") {
-        alert("Все поля должны быть заполнены!");
-        return;
-      }
-    }
-  
-    const formData = new FormData(e.currentTarget);
-  
-    formData.forEach((b, c) => (form1[c] = b));
-  
-    forms.form.reset();
-  
-    console.log(form1);
-  }
-
-
-=======
   const form = document.querySelector(".login-form");
   form.addEventListener("submit", handleSubmit);
   function handleSubmit(event) {
@@ -43,4 +13,3 @@ const forms = {
     console.log(form);
     event.currentTarget.reset();
   }
->>>>>>> Stashed changes
