fd85b4380dbd282888c02aec07ff3cdf135606ff
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 10:31:54 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,4 @@
-<<<<<<< Updated upstream
-const textInput = document.querySelector("#validation-input");
-const validNumber = Number(textInput.dataset.length);
-textInput.addEventListener("focus", () => { 
-  });
-  
-textInput.addEventListener("blur", () => {
-    if (textInput.value.length !== validNumber) {
-        textInput.classList.add("invalid");
-    }  else {
-        textInput.classList.remove("invalid")
-        textInput.classList.add("valid");
-    }
-=======
+
 const input = document.querySelector("#validation-input");
 const validLength = Number(input.dataset.length);
 input.addEventListener("focus", () => { 
@@ -24,5 +11,3 @@ input.addEventListener("blur", () => {
         input.classList.remove("invalid")
         input.classList.add("valid");
     }
->>>>>>> Stashed changes
-  });
