e9b3bb743424bc84c27808084fdf1e29711ee9ac
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 10:23:20 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 const categories = document.querySelector("#categories");
 const items = categories.querySelectorAll(".item");
 const itemsArr = Array.from(items);
@@ -10,17 +10,3 @@ for (const itemEl of itemsArr) {
 
     const listEl = itemEl.querySelectorAll("li");
     console.log("Elements:", listEl.length)
-=======
-const categories = document.querySelector("#categories");
-const items = categories.querySelectorAll(".item");
-const itemsArr = Array.from(items);
-console.log("Number of categories:", itemsArr.length);
-
-for (const itemEl of itemsArr) {
-    const headerEl = itemEl.querySelector("h2");
-    console.log("Category:", headerEl.textContent); 
-
-    const listEl = itemEl.querySelectorAll("li");
-    console.log("Elements:", listEl.length)
->>>>>>> Stashed changes
-  }
